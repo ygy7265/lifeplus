c7b89327dc982ba72824e27cd97b795c131eacf0
윤경엽(Yun Gyeongyeop) <48234811+ygy7265@users.noreply.github.com>
2023-11-06 11:34:21 +0900

    리액트 스프링부트 배포

--- src/main/java/com/example/lifeplus/LifeplusApplication.java
+++ src/main/java/com/example/lifeplus/LifeplusApplication.java
@@ -2,8 +2,14 @@ package com.example.lifeplus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+
+@SpringBootApplication(exclude = {
+        MongoAutoConfiguration.class,
+        DataSourceAutoConfiguration.class
+})
 public class LifeplusApplication {
 
     public static void main(String[] args) {
